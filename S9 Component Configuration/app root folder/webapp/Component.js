63cbd55879f29fff94a4921d53158dadf722d8ff
robergit88 <86879958+robergit88@users.noreply.github.com>
2024-09-12 17:51:05 +0200

    Actualizar Component.js

--- S9 Component Configuration/app root folder/webapp/Component.js
+++ S9 Component Configuration/app root folder/webapp/Component.js
@@ -43,26 +43,15 @@ sap.ui.define([
    });
 });
 
-// Creamos un archivo "Component.js" en la carpeta de la aplicación web que contendrá
+// El archivo "Component.js" contendrá
 // la configuración de nuestra aplicación.
-// SAPUI5 invoca automáticamente la función de inicio del componente cuando se crea una instancia
-// del componente.
+// SAPUI5 invoca automáticamente la función 
+// de inicio del componente cuando se crea 
+// una instancia del componente.
 // Nuestro componente hereda de la clase base sap/ui/core/UIComponent, y es obligatorio realizar
-// la llamada súper a la función init de la clase base en el método init anulado.
+// la llamada súper a la función init de la
 
-// El archivo Component.js ahora consta de dos partes: la nueva sección de metadatos y la función init
-// introducida anteriormente que se llama cuando se inicializa el componente.
-
-// La sección de metadatos define una referencia a la vista raíz, de modo que en lugar de mostrar la vista raíz
-// directamente en el archivo index.js como lo hicimos anteriormente, el componente ahora administra
-// la visualización de la vista de la aplicación.
-// También implementa la interfaz sap.ui.core.IAsyncContentCreation, que permite crear el componente
-// de forma totalmente asíncrona.
-
-// En la función init creamos una instancia de nuestro modelo de datos y el modelo i18n como lo hicimos antes
-// en el controlador de la aplicación. Tenga en cuenta que los modelos se configuran directamente
-// en el componente y no en la vista raíz del componente. Sin embargo, como los controles anidados heredan
-// automáticamente los modelos de sus controles principales, los modelos también están disponibles en la vista.
+//clase base en el método init anulado.
 
 
 
