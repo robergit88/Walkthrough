319acea440d21393c2d4ad3887cb904294ffddd3
robergit88 <86879958+robergit88@users.noreply.github.com>
2024-09-14 02:34:16 +0200

    Actualizar HelloPanel.controller.js

--- S16 Dialogs and Fragments/app root folder/webapp/controller/HelloPanel.controller.js
+++ S16 Dialogs and Fragments/app root folder/webapp/controller/HelloPanel.controller.js
@@ -8,36 +8,24 @@ sap.ui.define([
         
        onShowHello() {
 
-          // read msg from i18n model
-          const oBundle = this.getView().getModel("i18n").getResourceBundle();
-          const sRecipient = this.getView().getModel().getProperty("/recipient/name");
-          const sMsg = oBundle.getText("helloMsg", [sRecipient]);
+// read msg from i18n model
+const oBundle = this.getView().getModel("i18n").getResourceBundle();
+
+const sRecipient = this.getView().getModel().getProperty("/recipient/name");
+
+const sMsg = oBundle.getText("helloMsg", [sRecipient]);
  
-          // show message
-          MessageToast.show(sMsg);
+// show message
+   MessageToast.show(sMsg);
        },
 
-       async onOpenDialog() {
-         // create dialog lazily
-         this.oDialog ??= await this.loadFragment({
-             name: "ui5.walkthrough.view.HelloDialog"
-         });
+async onOpenDialog() {
+
+// create dialog lazily
+this.oDialog ??= await this.loadFragment({
+             name: "ui5.walkthrough.view.HelloDialog" });
      
-         this.oDialog.open();
-     }
+ this.oDialog.open(); }
 
     });
  });
-
-//  Usando async/await, manejamos la apertura del diálogo de forma asincrónica cada vez que se activa 
-//  el evento.
-// Si el fragmento de diálogo aún no existe, se crea una instancia del fragmento llamando a la API 
-// loadFragment. Luego almacenamos el cuadro de diálogo en la instancia del controlador. 
-// Esto nos permite reutilizar el diálogo cada vez que se activa el evento.
-
-// TIP
-// Para reutilizar la funcionalidad de apertura y cierre de diálogos en otros controladores, 
-// puede crear un nuevo archivo ui5.walkthrough.controller.BaseController, 
-// que extiende sap.ui.core.mvc.Controller, y colocar toda la codificación relacionada 
-// con los diálogos en este controlador. Ahora, todos los demás controladores pueden extenderse 
-// desde ui5.walkthrough.controller.BaseController en lugar de sap.ui.core.mvc.Controller.
