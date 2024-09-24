750c2c712b801d49441ef847f54860d7b6c45f2a
robergit88 <86879958+robergit88@users.noreply.github.com>
2024-09-24 17:33:13 +0200

    foo

--- S27 Unit Test with QUnit/app root folder/webapp/test/unit/model/unitTests.qunit.js
+++ S27 Unit Test with QUnit/app root folder/webapp/test/unit/model/unitTests.qunit.js
@@ -7,10 +7,10 @@ sap.ui.require(["sap/ui/core/Core"], async(Core) => {
 
 	sap.ui.require([
 		// "http://sdk.openui5.org/resources/formatter"], () => {
-		// "unit/model/formatter"], () => {
-		// "test/unit/model/formatter"], () => {
+		// "../model/formatter"], () => {
+		"test/unit/model/formatter"], () => {
 		// "/webapp/test/unit/model/formatter"], () => {
-		"./webapp/test/unit/model/formatter"], () => {
+		// "./webapp/test/unit/model/formatter"], () => {
 		// "./model/formatter"], () => {
 
 		QUnit.start();
