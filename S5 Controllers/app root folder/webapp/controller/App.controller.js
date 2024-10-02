d4750d3d03201eff34a4de053faf9e37e1ecaaec
robergit88 <86879958+robergit88@users.noreply.github.com>
2024-10-03 00:16:09 +0200

    Actualizar App.controller.js

--- S5 Controllers/app root folder/webapp/controller/App.controller.js
+++ S5 Controllers/app root folder/webapp/controller/App.controller.js
@@ -1,18 +1,17 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
- ], (Controller) => {
+    "sap/ui/core/mvc/Controller"], 
+ (Controller) => {
+
     "use strict";
  
-    return Controller.extend("ui5.walkthrough.controller.App", {
-       onShowHello() {
-          // show a native JavaScript alert
-          alert("Hello World from controller class");
+ return Controller.extend("ui5.walkthrough.controller.App", 
+
+{
+  onShowHello() {
+
+  // show a native JavaScript alert
+  alert("Hello World from controller class");
        }
+
     });
  });
-
-// Definimos el controlador de la aplicación en su propio archivo extendiendo 
-// el archivo sap/ui/core/mvc/Controller proporcionado por UI5.
-
-// Al principio, contiene solo una función llamada onShowHello que maneja el evento de pulsación 
-// del botón mostrando una alerta.
