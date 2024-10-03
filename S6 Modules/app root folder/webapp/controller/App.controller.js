73b679648da24c2e86f13ed8c842d3ac1c3438a6
robergit88 <86879958+robergit88@users.noreply.github.com>
2024-10-03 06:22:14 +0200

    Actualizar App.controller.js

--- S6 Modules/app root folder/webapp/controller/App.controller.js
+++ S6 Modules/app root folder/webapp/controller/App.controller.js
@@ -5,24 +5,12 @@ sap.ui.define([
    "use strict";
 
    return Controller.extend("ui5.walkthrough.controller.App", {
-      onShowHello() {
+      
+onShowHello() {
 
-//       begin delete         
-//       Se muestra alerta de javascript nativo
-//          alert("Hello World");
-//       end delete
-
-         MessageToast.show("Hello World from message toast module");
+MessageToast.show("Hello World from message toast module");
       }
+
    });
 });
-
-// Ampliamos la gama de módulos necesarios con la ruta completa a sap/m/MessageToast.
-
-// Una vez cargados ambos módulos, Controller y MessageToast, se llama a la función de devolución de llamada y podemos hacer uso de ambos objetos accediendo a los parámetros pasados ​​a la función.
-
-// Esta sintaxis de Definición de Módulo Asincrónico (AMD) permite separar claramente la carga del módulo de la ejecución del código y mejora enormemente el rendimiento de la aplicación.
-
-// El navegador puede decidir cuándo y cómo se cargan los recursos antes de la ejecución del código.
-
 
