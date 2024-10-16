eeebdb2bf631d18c9675fc6ae3fddf7ed71b0a10
robergit88 <86879958+robergit88@users.noreply.github.com>
2024-10-16 06:59:25 +0200

    Actualizar index.js

--- 09 Component Configuration/app root folder/webapp/index.js
+++ 09 Component Configuration/app root folder/webapp/index.js
@@ -11,13 +11,3 @@ sap.ui.define([
 		async: true
 	}).placeAt("content");
 });
-
-// Ahora creamos un contenedor de componentes en lugar de la vista en nuestro index.js 
-// que crea una instancia de la vista según la configuración del componente.
-
-// convenciones
-//-------------
-// El componente se llama Component.js. 
-// Junto con todos los recursos de la interfaz de usuario de la aplicación, el componente se encuentra 
-// en la carpeta de la aplicación web. 
-// El archivo index.html se encuentra en la carpeta de la aplicación web si se utiliza de forma productiva.
